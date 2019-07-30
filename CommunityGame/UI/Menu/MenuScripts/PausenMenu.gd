f9f7d03b1e0781db614a781daac9fd00238a7ed4
HugeGameArtGD <hugegameartgd@gmail.com>
2019-07-30 22:16:58 +0200

    merge

--- CommunityGame/UI/Menu/MenuScripts/PausenMenu.gd
+++ CommunityGame/UI/Menu/MenuScripts/PausenMenu.gd
@@ -1,12 +1,26 @@
 # Pausenmenu Script
 extends Control
 
+#variable an der man erkennt ob das spiel pausiert ist
+var paused = false
+
 # Aktiviert, wenn Szene geladen wird
 func _ready():
+	
 	# Setzt den Pausenmodus auf inaktiv
-	pause_mode = false
+	get_tree().paused = false
 	
 	# Spielt Die Beginn-Animation ab
+	paused = false
+	
+#Aktiviert wenn Szene Geladen wird
+func _ready():
+	
+	#setzt denn pausen modus auf nicht aktiv
+	#get_tree().paused = false
+	pause_mode = false
+	
+	#Spielt Die "Beginn" Animation ab
 	$Anim.play("Beginn")
 	
 	# Deaktiviert die Knöpfe 
@@ -15,16 +29,29 @@ func _ready():
 	$CanvasLayer/HBoxContainer/VBoxContainer/Weiter.disabled = true
 	$CanvasLayer/HBoxContainer/VBoxContainer/Spiel_Beenden.disabled = true
 
+
 # Wird jeweils aktiviert, wenn ein Knopf gedrückt wird
 func _input(event):
+	
 	# Wenn Escape gedrückt wird
 	if Input.is_action_just_pressed("ui_cancel"):
+
 		# Wenn bisher noch nicht pausiert wurde, wird jedenfalls jetzt das Pausenmenu geöffnet
+		if paused == false:
+			PauseEntered()
+			
+			#macht pause auf true was heißt das spiel ist pausiert
+			paused = true
+		
+		# Wenn bisher schon das Pausenmenu offen war, dann wird jetzt nicht mehr pausiert
 		if not pause_mode:
 			PauseEntered()
-		# Wenn bisher schon das Pausenmenu offen war, dann wird jetzt nicht mehr pausiert
+		#wenn pause_mode ist richtig wird das pausen menü geschlossen
 		else:
 			PauseExited()
+			
+			#macht pause auf false was heißt das spiel ist nicht pausiert
+			paused = false
 
 
 # Aktiviert, wenn aufgerufen
@@ -40,6 +67,8 @@ func PauseEntered():
 	$CanvasLayer/HBoxContainer/VBoxContainer/Spiel_Beenden.disabled = false
 	
 	# Setzt den Pausenmodus auf aktiv
+	get_tree().paused = true
+	#get_tree().paused = true
 	pause_mode != pause_mode
 	get_node("/root/Main/TestLevel/Player").set_physics_process(false)
 
@@ -56,16 +85,19 @@ func PauseExited():
 	$CanvasLayer/HBoxContainer/VBoxContainer/Spiel_Beenden.disabled = true
 	
 	# Setzt den Pausenmodus auf inaktiv
+	get_tree().paused = false
+	#get_tree().paused = false
 	pause_mode != pause_mode
 	get_node("/root/Main/TestLevel/Player").set_physics_process(true)
 
 # Wird aktiv, wenn "Weiter" gedrückt wird
 func _on_Weiter_pressed():
+	
 	#Called Funktion PauseExited()
 	PauseExited()
 
 
-# Wird aktiv, wenn Spiel_Beenden gedrückt wird
+# Wird aktiv, wenn "Spiel_Beenden" gedrückt wird
 func _on_Spiel_Beenden_pressed():
 	# Speichert und beendet das Spiel
 	# TODO: Speichere den Fortschritt
@@ -73,9 +105,10 @@ func _on_Spiel_Beenden_pressed():
 	#Beendet das Spiel
 	get_tree().quit()
 
+
 # Wird aktiv, wenn "Options" gedrückt wird
 func _on_Options_pressed():
-	# Wechselt zum Settings-Menü
+	# Wechselt zum Settings-Menu
 	get_tree().change_scene("res://Settings/Settings.tscn")
 
 
